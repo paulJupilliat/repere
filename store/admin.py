a704cbf70bd375da22bcdfe1211f93b8ccc910ca
paulJupilliat <jupilliatp@gmail.com>
2023-07-22 12:43:00 +0200

    lien models -> vue

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from store.models import Products
+
 
 # Register your models here.
+admin.site.register(Products)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Products
+
 
 # Create your views here.
+def index(request):
+    products = Products.objects.all()
+    return render(request, 'store/index.html', context={'products': products})
