5657f06be422e35c55921681633bb5d4ea8e6d8a
paulJupilliat <jupilliatp@gmail.com>
2023-07-31 11:10:57 +0200

    gestion du prix total et de l'adress de livraison -> à ajouter en base une fois le payment validé + les articles de la commandes

--- repere/urls.py
+++ repere/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from accounts.views import logout_user, signup, login_user
-from store.views import index, product_detail, add_to_cart, cart, delete_cart
+from store.views import index, product_detail, add_to_cart, cart, delete_cart, payment_page
 from django.conf.urls.static import static 
 from repere import settings
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path('cart/',cart, name='cart'),
     path('cart/delete/', delete_cart, name='delete_cart'),
     path('login/', login_user, name='login'),
+    path('paiement/', payment_page, name='payment_page'),
     
     path('product/<str:slug>/', product_detail, name='product'),
     path('product/<str:slug>/add-to-cart', add_to_cart, name='add_to_cart'),
--- store/views.py
+++ store/views.py
@@ -5,6 +5,8 @@ from store.models import Cart, Products, Order, Commande
 
 
 # Create your views here.
+
+
 def index(request):
     products = Products.objects.all()
     return render(request, 'store/index.html', context={'products': products})
@@ -17,24 +19,28 @@ def product_detail(request, slug):
 def add_to_cart(request, slug):
     # si je ne suis pas connecter, ca me renvoie sur la page de connexion
     if not request.user.is_authenticated:
-        return render(request, 'accounts/login.html')
-    #les différents cas de figure : deja un panier, pas de panier, particle existe deja dans son panier
-    user = request.user
-    product = get_object_or_404(Products, slug=slug)
-    #methode permettant de recuperer si existe, sinon créer
-    cart, _ = Cart.objects.get_or_create(user=user)
-    order, created = Order.objects.get_or_create(user=user, product=product)
-    if created : 
-        cart.orders.add(order)
-        cart.save()
+        return redirect('login')
     else:
-        order.quantity += 1
-        order.save()
-    return redirect(reverse("product", kwargs={'slug': slug}))
+        #les différents cas de figure : deja un panier, pas de panier, particle existe deja dans son panier
+        user = request.user
+        product = get_object_or_404(Products, slug=slug)
+        #methode permettant de recuperer si existe, sinon créer
+        cart, _ = Cart.objects.get_or_create(user=user)
+        order, created = Order.objects.get_or_create(user=user, product=product)
+        if created : 
+            cart.orders.add(order)
+            cart.save()
+        else:
+            order.quantity += 1
+            order.save()
+        return redirect(reverse("product", kwargs={'slug': slug}))
 
 def cart(request):
     cart = get_object_or_404(Cart, user=request.user)
-    return render(request, 'store/cart.html', context={'orders': cart.orders.all()})
+    prix = 0  # Initialisez le prix total à 0
+    for order in cart.orders.all():  # Itérez sur toutes les commandes dans le panier
+        prix += order.product.price * order.quantity  # Accumulez le prix total
+    return render(request, 'store/cart.html', context={'orders': cart.orders.all(), 'prix': prix })
 
 def delete_cart(request):
     cart = request.user.cart
@@ -42,6 +48,15 @@ def delete_cart(request):
         cart.orders.all().delete()
         cart.delete()
     return redirect('index')
+
+def payment_page(request):
+    order = get_object_or_404(Order, user=request.user)
+    cart = get_object_or_404(Cart, user=request.user)  # Obtenez le panier de l'utilisateur
+    
+    prix = 0  # Initialisez le prix total à 0
+    for order in cart.orders.all():  # Itérez sur toutes les commandes dans le panier
+        prix += order.product.price * order.quantity  # Accumulez le prix total
+    return render(request, 'store/payment.html', context={'order': order, 'prix': prix})
 
 
 def commande(request):
